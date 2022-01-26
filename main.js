c4dd8968615979ce986fa22b26bf4682cdefe034
GetMeUsername <84090181+GetMeUsername@users.noreply.github.com>
2022-01-26 19:37:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -139,3 +139,43 @@ function my_keydown(e)
         console.log("c");
     }
 }
+function up(){
+     if(player_y >=0)
+     {
+         player_y = player_y - block_image_height;
+         console.log("block image height = " =+ block_image_height);
+         console.log("When Up arrow key is pressed, X ="+ player_x + ", Y = "+player_x);
+         canvas.remove(player_object);
+         player_update();
+     }
+}
+function down(){
+    if(player_y >=500)
+    {
+        player_y = player_y + block_image_height;
+        console.log("block image height = " = + block_image_height);
+        console.log("When Down arrow is pressed, X"+player_x+", Y = "+player_x);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function left(){
+    if(player_x >=0)
+    {
+        player_x = player_x - block_image_width;
+        console.log("block image width =" = + block_image_width);
+        console.log("When Left arrow is pressed, Y="+player_y+", X ="+player_x)
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function right(){
+    if(player_x >=850)
+    {
+        player_x = player_x + block_image_width;
+        console.log("block image width =" = + block_image_width);
+        console.log("When Right arrow is pressed, Y="+player_y+", X ="+player_x)
+        canvas.remove(player_object);
+        player_update();
+    }
+}
